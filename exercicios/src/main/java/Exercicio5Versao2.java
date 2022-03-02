083a0707e4334b55857da12f7f2bcb4f41e7131e
Caroliny Abreu <caroliny.a.t@hotmail.com>
2022-03-02 09:56:52 -0300

    Resolvendo exercício 5

---
+++ exercicios/src/main/java/Exercicio5Versao2.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio5Versao2 {
+    public static void main(String[] args) {
+        Scanner valor = new Scanner(System.in);
+
+        //Número aleatório
+        Random rand = new Random();
+        int x = rand.nextInt(10) + 1;
+
+        int num;
+
+        System.out.println("Insira um valor inteiro de 1 a 10: ");
+        num = valor.nextInt();
+        while(num != x){
+            System.out.println("Valor incorreto, tente outra vez");
+            num = valor.nextInt();
+        }
+        System.out.println("Valor correto!");
+        valor.close();
+    }
+}
+
+
---
+++ exercicios/src/main/java/Exercício5.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercício5 {
+    public static void main(String[] args) {
+        Scanner valor = new Scanner(System.in);
+
+        //Número aleatório
+        Random rand = new Random();
+        int x = rand.nextInt(10) + 1;
+
+        int num;
+
+        System.out.println("Insira um número inteiro de 1 a 10: ");
+        do {
+            num = valor.nextInt();
+        }while(num != x);
+        System.out.println("Valor correto!");
+
+        valor.close();
+    }
+}
