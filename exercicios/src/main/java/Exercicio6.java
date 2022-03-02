89b67374f1b95b7a85b42c4f31b8617ad12a04ae
Caroliny Abreu <caroliny.a.t@hotmail.com>
2022-03-02 10:12:53 -0300

    Update exercício6

--- exercicios/src/main/java/Exercicio6.java
+++ exercicios/src/main/java/Exercicio6.java
@@ -5,7 +5,7 @@ public class Exercicio6 {
     public static void main(String[] args) {
         Scanner valor = new Scanner(System.in);
 
-        //Número aleatório
+        //Gerando um número aleatório entre 1 a 10
         Random rand = new Random();
         int x = rand.nextInt(10) + 1;
 
@@ -14,13 +14,13 @@ public class Exercicio6 {
         System.out.println("Insira um número inteiro de 1 a 10: ");
         do {
             num = valor.nextInt();
-            if(num>x) {
-                System.out.println("Número digitado maior que o gerado pelo computador!");
+            if(num > x) {
+                System.out.println("Número digitado maior que o gerado!");
             }
             else {
-                System.out.println("Número digitado menor que o gerado pelo computador!");
+                System.out.println("Número digitado menor que o gerado!");
             }
-            }while(num != x);
+            }while(num != x); 
 
         System.out.println("Valor correto!");
 
