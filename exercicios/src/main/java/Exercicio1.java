97eaafd7ae5811d804307d19a34caf56ee03f2e0
Caroliny Abreu <caroliny.a.t@hotmail.com>
2022-03-02 08:33:30 -0300

    Resolvendo exercício 1

---
+++ exercicios/src/main/java/Exercicio1.java
@@ -0,0 +1,12 @@
+public class Exercicio1 {
+
+        public static void main(String[] args) {
+            int numZumbis1 = 120;
+            int numZumbis2 = 240;
+            int numZumbis3 = 320;
+            int totalZumbis = numZumbis1 + numZumbis2 + numZumbis3;
+
+            System.out.println("Total de Zumbis: " + totalZumbis);
+            System.out.println("Média de Zumbis mortos: " + totalZumbis/3);
+        };
+}
